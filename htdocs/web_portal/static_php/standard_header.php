c37aa36adb7ea4229cf705e9c907d65cbb90ad6c
Tom Byrne <tom.byrne@stfc.ac.uk>
2016-01-05 13:25:09 +0000

    added calendar to stylesheet

--- htdocs/web_portal/static_php/standard_header.php
+++ htdocs/web_portal/static_php/standard_header.php
@@ -15,6 +15,7 @@ function get_standard_header($title = null) {
         <link rel="SHORTCUT ICON" href="'.GocContextPath::getPath().'img/Logo-1.4-FavIcon-32x32.regional.ico" />
         <script type="text/javascript" src="'.GocContextPath::getPath().'javascript/jquery/jquery-1.10.min.js"></script>
         <script type="text/javascript" src="'.GocContextPath::getPath().'javascript/moment/moment.min.js"></script>
+        <script type="text/javascript" src="'.GocContextPath::getPath().'javascript/fullcalendar/fullcalendar.min.js"></script>
         <script type="text/javascript" src="'.GocContextPath::getPath().'javascript/bootstrap/js/bootstrap-3.1.min.js"></script>
         <script type="text/javascript" src="'.GocContextPath::getPath().'javascript/bootstrap/js/bootstrap-select.min.js"></script>
         <script type="text/javascript" src="'.GocContextPath::getPath().'javascript/datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
@@ -24,6 +25,7 @@ function get_standard_header($title = null) {
         <link rel="stylesheet" href="'.GocContextPath::getPath().'javascript/bootstrap/css/bootstrap.css" />
         <link rel="stylesheet" href="'.GocContextPath::getPath().'javascript/datetimepicker/css/bootstrap-datetimepicker.min.css" />
         <link rel="stylesheet" href="'.GocContextPath::getPath().'javascript/bootstrap/css/bootstrap-select.min.css" />
+        <link rel="stylesheet" href="'.GocContextPath::getPath().'javascript/fullcalendar/fullcalendar.min.css" />
         <link rel="stylesheet" type="text/css" href="'.GocContextPath::getPath().'css/web_portal.css" />
     <link rel="stylesheet" type="text/css" href="'.GocContextPath::getPath().'css/multiple-select.css"/>
     </head>
