1e8d0b8e62b09bdfbe0b44f2ccb1689d81aeff92
Tom Byrne <tom.byrne@stfc.ac.uk>
2016-02-03 10:59:17 +0000

    Reworked tooltip to use JS moments, and comments

--- htdocs/web_portal/controllers/downtime/downtimes_calendar.php
+++ htdocs/web_portal/controllers/downtime/downtimes_calendar.php
@@ -1,13 +1,13 @@
 <?php
 /*______________________________________________________
  *======================================================
- * File: view_downtime.php
- * Author: James McCarthy
- * Description: Retrieves and draws the data for a downtime
+ * File: downtimes_calendar.php
+ * Author: Tom Byrne
+ * Description: Retrieves and draws the data for the downtime calendar
  *
  * License information
  *
- * Copyright � 2009 STFC
+ * Copyright � 2016 STFC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,266 +20,252 @@
  *
  /*====================================================== */
 
-
-function getDowntimesAsJSON(){
+//Uses the GOCDBPI to return a list of downtimes as JSON, based on the params of the request
+function getDowntimesAsJSON()
+{
 
     $filterParams = array();
 
+    //if no date window is specified, just get the next 30 days
     $windowStart = date("Y-m-d");
-    $windowEnd = date_add(date_create(date("Y-m-d")), date_interval_create_from_date_string(30 .' days'))->format('Y-m-d');
+    $windowEnd = date_add(date_create(date("Y-m-d")), date_interval_create_from_date_string(30 . ' days'))->format('Y-m-d');
 
-
-    if(isset($_GET["start"])){
+    //If the filter params are specified in the request, add them to the filter param array
+    if (isset($_GET["start"]))
         $windowStart = date("Y-m-d", strtotime($_GET["start"]));
-    }
 
-    if(isset($_GET["end"])){
+    if (isset($_GET["end"]))
         $windowEnd = date("Y-m-d", strtotime($_GET["end"]));
-    }
 
-
-    if(!empty($_GET["classification"]))
+    if (!empty($_GET["classification"]))
         $filterParams['classification'] = $_GET["classification"];
 
-    if(!empty($_GET["severity"]))
+    if (!empty($_GET["severity"]))
         $filterParams['severity'] = $_GET["severity"];
 
-    if(!empty($_GET["production"]))
+    if (!empty($_GET["production"]))
         $filterParams['production'] = $_GET["production"];
 
-    if(!empty($_GET["monitored"]))
+    if (!empty($_GET["monitored"]))
         $filterParams['monitored'] = $_GET["monitored"];
 
-    if(!empty($_GET["certStatus"]))
+    if (!empty($_GET["certStatus"]))
         $filterParams['certification_status'] = $_GET["certStatus"];
 
     $selectedScopes = "";
-    if(!empty($_GET['scope'])){
+    if (!empty($_GET['scope'])) {
         $selectedScopes = $_GET['scope'];
     }
 
     $selectedServiceTypes = "";
-    if(!empty($_GET['service_type'])){
+    if (!empty($_GET['service_type'])) {
         $selectedServiceTypes = $_GET['service_type'];
     }
 
     $selectedSites = "";
-    if(!empty($_GET['site'])) {
+    if (!empty($_GET['site'])) {
         $selectedSites = $_GET['site'];
     }
 
     $selectedNGIs = "";
-    if(!empty($_GET['ngi'])) {
+    if (!empty($_GET['ngi'])) {
         $selectedNGIs = $_GET['ngi'];
     }
 
     $filterParams['windowstart'] = $windowStart;
     $filterParams['windowend'] = $windowEnd;
 
-    if($selectedScopes != "null" && $selectedScopes != "") {
+    if ($selectedScopes != "null" && $selectedScopes != "") {
         $filterParams['scope'] = $selectedScopes;
         $filterParams['scope_match'] = 'any';
     }
 
-    if($selectedServiceTypes != "null" && $selectedServiceTypes != "" ) {
+    if ($selectedServiceTypes != "null" && $selectedServiceTypes != "") {
         $filterParams['service_type_list'] = $selectedServiceTypes;
     }
 
-    if($selectedSites != "null" && $selectedSites != "" ) {
+    if ($selectedSites != "null" && $selectedSites != "") {
         $filterParams['sitelist'] = $selectedSites;
     }
 
-    if($selectedNGIs != "null" && $selectedNGIs != "" ) {
+    if ($selectedNGIs != "null" && $selectedNGIs != "") {
         $filterParams['ngilist'] = $selectedNGIs;
     }
 
+    //create the downtime service, and pass the filter array to getDowntimesFilterByParams
     $dtServ = \Factory::getDowntimeService();
+    //this returns an array of doctrine downtime objects
     $downtimes = $dtServ->getDowntimesFilterByParams($filterParams);
-    //$downtimes = $dt->getActiveAndImminentDowntimes($windowStart, $windowEnd);
 
-//    print_r(var_dump($downtimes));
-//    die("force die");
-
+    //initialise the array that will be our JSON structure
     $downtimesArray = array();
 
-    foreach($downtimes as $dt){
-        //print_r(var_dump($dt['id']));
-        //continue;
+    //for each doctrine downtime obj, extract or get the data we want, and create the structure
+    foreach ($downtimes as $dt) {
         $tempDowntime = array();
         $tempDowntime['id'] = $dt['id'];
         $tempDowntime['site'] = $dtServ->getDowntime($tempDowntime['id'])->getServices()->first()->getParentSite()->getName();
-        $tempDowntime['title'] = "<b>"  . $tempDowntime['site'] . "</b>" . ": " . utf8_encode($dt['description']);
-        //$tempDowntime['title'] = utf8_encode($dt['description']);
+        $tempDowntime['title'] = "<b>" . $tempDowntime['site'] . "</b>" . ": " . utf8_encode($dt['description']);
         $tempDowntime['start'] = $dt['startDate']->format('c');
         $tempDowntime['end'] = $dt['endDate']->format('c');
         $tempDowntime['severity'] = $dt['severity'];
         $tempDowntime['class'] = $dt['classification'];
-        //$tempDowntime['end'] = $dt->getEndDate()->format('c');
         $tempDowntime['url'] = "index.php?Page_Type=Downtime&id=" . $tempDowntime['id'];
 
 
         $downtimesArray[] = $tempDowntime;
     }
-    header("Content-type:text/json");
-    //print_r(var_dump(json_encode($downtimesArray)));
-    //print_r(var_dump($downtimesArray));
-    //die("force die");
 
-    //throw here if false
+
+    //set the header
+    header("Content-type:text/json");
+    //return the json encoded array
     return json_encode($downtimesArray);
 }
 
-function getTooltip(){
-    if(isset($_GET['downtimeID'])){
+//this function grabs the data needed for the dontime calendar tooltips, and calls the tooltip view
+function getTooltip()
+{
+
+    //downtimeID must be set
+    if (isset($_GET['downtimeID'])) {
         $downtime = \Factory::getDowntimeService()->getDowntime($_GET['downtimeID']);
     } else {
         throw new \Exception("downtime" . $_GET['downtimeID'] . "does not exist.");
     }
+
+    //initialise the parameter array to be passed through to the view
     $params = array();
     $start = $downtime->getStartDate();
     $end = $downtime->getEndDate();
 
-    $params['duration'] = date_diff($start, $end)->days . " days " . date_diff($start, $end)->h . " hours";
-    //$params['endsIn'] = date_diff(date_create(), $end)->days . " days " . date_diff($start, $end)->h . " hours";
-
-
-
-    $params['start'] = $downtime->getStartDate()->format('d-m-Y H:i');
-    $params['end'] = $downtime->getEndDate()->format('d-m-Y H:i');
+    //populate the parameter array
+    $params['id'] = $_GET['downtimeID'];
+    //$params['duration'] = date_diff($start, $end)->days . " days " . date_diff($start, $end)->h . " hours";
+    $params['start'] = $downtime->getStartDate()->format(DATE_ATOM);
+    $params['end'] = $downtime->getEndDate()->format(DATE_ATOM);
     $params['description'] = $downtime->getDescription();
     $params['services'] = array();
     $params['scopes'] = array();
-
-
     $params['site'] = $downtime->getServices()->first()->getParentSite()->getName();
 
-    //throw new \Exception(var_dump($dt->getServices()->first()->getScopes());
-    foreach($downtime->getServices() as $service){
+    //we need to get a list of all services, and then a list of all of those services scopes
+    foreach ($downtime->getServices() as $service) {
         array_push($params['services'], $service->getHostName());
-        //array_push($params['sites'], $service->getParentSite()->getName());
-        foreach($service->getScopes() as $scope){
+        foreach ($service->getScopes() as $scope) {
             array_push($params['scopes'], $scope->getName());
         };
     };
 
+    //which we then remove duplicates from
     $params['services'] = array_unique($params['services']);
-    $params['scopes'] = implode( ", " ,array_unique($params['scopes']));
+    $params['scopes'] = implode(", ", array_unique($params['scopes']));
 
+    //this gives us out "x of y services affected" line
     $params['affected'] = "<b>" . count($params['services']) . "</b> of <b>" . count($downtime->getServices()->first()->getParentSite()->getServices()) . "</b>";
 
     //shorten the services array to a sensible length so the tooltip fits on screen
-    if (count($params['services']) > 10){
+    if (count($params['services']) > 10) {
         $params['services'] = array_slice($params['services'], 0, 10);
         array_push($params['services'], "...");
     }
 
-
-    //$params['sites'] = array_unique($params['sites']);
-    //throw new \Exception(var_dump($params));
-
+    //call the view
     show_view("downtime/downtimes_calendar_tooltip.php", $params, null, true);
 }
 
 
-function view() {
+function view()
+{
     require_once __DIR__ . '/../utils.php';
     require_once __DIR__ . '/../../../web_portal/components/Get_User_Principle.php';
 
-//    $dt = \Factory::getDowntimeService();
-//
-//    $downtimeArray = $dt->getDowntimesFilterByParams(null);
-//    print_r($downtimeArray);
-//    die("force die");
-    $dn = Get_User_Principle();
-    $user = \Factory::getUserService()->getUserByPrinciple($dn);
-    $params['portalIsReadOnly'] = portalIsReadOnlyAndUserIsNotAdmin($user);
-    
-    //date_default_timezone_set("UTC");
-    
+//not necessary?
+//    $dn = Get_User_Principle();
+//    $user = \Factory::getUserService()->getUserByPrinciple($dn);
+//    $params['portalIsReadOnly'] = portalIsReadOnlyAndUserIsNotAdmin($user);
+
+
     $timePeriod = 1;
-    if(isset($_REQUEST['timePeriod'])) {
-    	$timePeriod = $_REQUEST['timePeriod'];
+    if (isset($_REQUEST['timePeriod'])) {
+        $timePeriod = $_REQUEST['timePeriod'];
     }
 
-    // URL mapping
-    // Return all scopes for the parent Site with the specified Id as a JSON object
-    // Used in ajax requests for display purposes
-    if(isset($_GET['getDowntimesAsJSON'])){
+    // Used by the calendar to get the downtimes
+    if (isset($_GET['getDowntimesAsJSON'])) {
         die(getDowntimesAsJSON());
     }
 
-    // URL mapping
-    // Return all scopes for the parent Site with the specified Id as a JSON object
-    // Used in ajax requests for display purposes
-    if(isset($_GET['getTooltip'])){
+    // Used by the tooltips to get the downtime info
+    if (isset($_GET['getTooltip'])) {
         die(getTooltip());
     }
 
+    //We need a list of scoped present to populate the select box
     $params['scopes'] = \Factory::getScopeService()->getScopes();
 
+    //This section is reading the parameters passed into the page from the user.
+    //these are used to select the starting values of the filter parameters on the calendar ui
+    //which in turn determine the query sent to getDowntimesAsJSON.
+
     $selectedScopes = array();
-    if(!empty($_GET['scope'])) {
+    if (!empty($_GET['scope'])) {
         $selectedScopes = explode(',', $_GET['scope']);
     }
 
     $selectedSites = array();
-    if(!empty($_GET['site'])) {
+    if (!empty($_GET['site'])) {
         $selectedSites = explode(',', $_GET['site']);
     }
 
     $selectedServiceTypes = array();
-    if(!empty($_GET['serviceType'])) {
+    if (!empty($_GET['serviceType'])) {
         $selectedServiceTypes = explode(',', $_GET['service_type']);
     }
 
     $selectedNGIs = array();
-    if(!empty($_GET['ngi'])) {
+    if (!empty($_GET['ngi'])) {
         $selectedNGIs = explode(',', $_GET['ngi']);
     }
 
-
-
     $severity = "ALL";
-    if(!empty($_GET['severity'])) {
+    if (!empty($_GET['severity'])) {
         $severity = $_GET['severity'];
     }
 
-
-
     $classification = "ALL";
-    if(!empty($_GET['classification'])) {
+    if (!empty($_GET['classification'])) {
         $classification = $_GET['classification'];
     }
 
     $certStatus = "ALL";
-    if(!empty($_GET['certStatus'])) {
+    if (!empty($_GET['certStatus'])) {
         $certStatus = $_GET['certStatus'];
     }
 
     $production = "ALL";
-    if(!empty($_GET['production'])) {
+    if (!empty($_GET['production'])) {
         $production = $_GET['production'];
     }
 
     $monitored = "ALL";
-    if(!empty($_GET['monitored'])) {
+    if (!empty($_GET['monitored'])) {
         $monitored = $_GET['monitored'];
     }
 
     $date = date("Y-m-d");
 
-    if(!empty($_GET['date'])) {
-
-        $date = date( "Y-m-d", strtotime($_GET['date']));
+    if (!empty($_GET['date'])) {
+        $date = date("Y-m-d", strtotime($_GET['date']));
     }
 
     $view = "basicWeek";
 
-    if(!empty($_GET['view'])) {
-
+    if (!empty($_GET['view'])) {
         $view = $_GET['view'];
     }
 
+    //construct the params to pass to the view
     $params['selectedScopes'] = $selectedScopes;
     $params['severity'] = $severity;
     $params['classification'] = $classification;
@@ -303,6 +289,7 @@ function view() {
     $siteFilterParams = array();
     $siteFilterParams['scope'] = '';
     $params['sites'] = $serv->getSitesFilterByParams($siteFilterParams);
-    //throw new \Exception(var_dump($params));
+
+
     show_view("downtime/downtimes_calendar.php", $params, "Downtimes Calendar");
 }
--- htdocs/web_portal/views/downtime/downtimes_calendar_tooltip.php
+++ htdocs/web_portal/views/downtime/downtimes_calendar_tooltip.php
@@ -1,6 +1,6 @@
 <?php
 define('DATE_FORMAT', 'd-m-Y H:i');
-$duration = $params['duration'];
+//$duration = $params['duration'];
 $start = $params['start'];
 $end = $params['end'];
 $services = $params['services'];
@@ -8,6 +8,20 @@ $scopes = $params['scopes'];
 $site = $params['site'];
 $description = $params['description'];
 $affected = $params['affected'];
+
+
+
+
+//I've elected to handle the time section in js, as moment.js makes this really simple
+//however, I need to generate a unique id for each qtip's timefield so I don't replace each qtip's
+//timeField with the most recently generated one's text
+//
+//
+//due to the way the tooltips are generated, the timefield-<id> identifier will not be unique, as a
+//new qtip is generated for the same event if an event spans two or more views (and you change the view), so we need to add some more uniqueness using rand()
+$id = $params['id'];
+$randomNumber = rand();
+
 ?>
 
 <!--This page is the html for the downtime calendar tooltips-->
@@ -20,9 +34,7 @@ $affected = $params['affected'];
 
     <hr class="tooltip-hr"/>
 
-    <p class="tooltip-p">
-        <b>Duration:</b>
-        <?php echo $duration;?>
+    <p id="timeField-<?php echo $id . "-" . $randomNumber;?>" class="tooltip-p">
     </p>
 
     <hr class="tooltip-hr"/>
@@ -47,4 +59,32 @@ $affected = $params['affected'];
     </p>
 
 </div>
+
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        var startTime = moment(<?php echo( "\"". $start . "\"");?>);
+        var endTime = moment(<?php echo( "\"". $end . "\"");?>);
+        var nowTime = moment();
+        var duration = moment.duration(endTime.diff(startTime));
+        var timeField = "#timeField-<?php echo $id . "-" . $randomNumber;?>";
+
+        var timeString = "";
+        //inbetween
+        if (moment(nowTime).isBetween(startTime, endTime)){
+            timeString =  "Ongoing, ending " + moment(endTime).fromNow();
+        //future
+        } else if (moment(startTime).isAfter(nowTime)){
+            timeString =  "Starting " + moment(startTime).fromNow() + "<br/> Lasting " + duration.humanize();
+        //past
+        } else {
+            timeString =  "Ended " + moment(endTime).fromNow() + "<br/> Lasted " + duration.humanize();
+        }
+
+        $(timeField).html(timeString);
+
+    });
+
+
+</script>
 </html>
