cae2920b62d39bf6fd5036a10a5fef1d9c9c6a85
James Adams <james.adams@stfc.ac.uk>
2018-03-02 11:15:11 +0000

    Use built-in PHP syntax checker on modified PHP files
    Don't run if no files were modified.

---
+++ .travis/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $TRAVIS ]]; then
+    cd "$TRAVIS_BUILD_DIR" || exit 2
+
+    # Check modified PHP files with PHP's internal syntax checker
+    git diff --name-only HEAD^ | grep '\.php$' | xargs -r -n 1 php -l || exit 1
+
+    # Run test suite
+    vendor/bin/phpunit tests/DoctrineTestSuite1.php
+else
+    echo 'ABORTED: NOT RUNNING ON TRAVIS'
+    exit 2
+fi
