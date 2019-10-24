4f83a48d41b094547b6edd965b3bbe3ddb773f7e
George Ryall <George.ryall@stfc.ac.uk>
2019-10-24 17:30:05 +0100

    Update STFC logos to reflect new branding
    This update replaces the old STFC logo with the new version. There is a
    cropped version for the portal, which is likley technicallly not in line
    with brand guidlines. However, the old cropped logo it replaces was not
    in line with the old guidlines either...

--- htdocs/web_portal/components/Draw_Components/draw_page_components.php
+++ htdocs/web_portal/components/Draw_Components/draw_page_components.php
@@ -123,7 +123,7 @@
         $HTML = "";
         $HTML .= '<div class="Left_Logo_Box left_box_menu">';
         $HTML .= '<div class="Left_Logo_Row">';
-        $HTML .= '<a href="https://stfc.ukri.org/" target="_blank"><img class="Left_Logo_Image" style="margin: 0px 5px 0px 0px;" src="'.\GocContextPath::getPath().'img/STFCMediumColourTrans_crop.png" alt="The logo of the Science and Technology Facilities Council" /></a>';
+        $HTML .= '<a href="https://stfc.ukri.org/" target="_blank"><img class="Left_Logo_Image" style="margin: 0px 5px 0px 0px;" src="'.\GocContextPath::getPath().'img/UKRI_STF_Council-Logo_Horiz-RGB_crop.png" alt="The logo of the Science and Technology Facilities Council" /></a>';
         $HTML .= '<a href="https://europa.eu/european-union/index_en" target="_blank"><img class="Left_Logo_Image" src="'.\GocContextPath::getPath().'img/eu_flag_yellow_low_150.png" alt="The logo of the European Union" /></a>&nbsp;&nbsp;';
         $HTML .= '<a href="https://www.egi.eu" target="_blank"><img class="Left_Logo_Image" src="'.\GocContextPath::getPath().'img/egi_logo_no_background_150.png" alt="The logo of the E G I Foundation" /></a>';
         $HTML .= '<a href="https://www.eosc-hub.eu/" target="_blank"><img class="Left_Logo_Image" style="margin: 0px 0px 0px 5px" src="'.\GocContextPath::getPath().'img/eosc-hub-v-web_150.png" alt="The logo of the EOSC-hub Horizon 20 20 project" /></a>';
