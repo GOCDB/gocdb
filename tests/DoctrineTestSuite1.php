33ba316f08d343a2cb1770066c9be2bca24fc96b
George Ryall <George.ryall@stfc.ac.uk>
2020-07-16 22:46:28 +0100

    Increase php memory limit for test suite
    Most of our enviroments (including travis) seem to default to (or we set
    somewhere else) a memory limit of 128M. The latest additions to the test
    suite nudge it over this limit. This change ups the limit
    when running the test suite to 512M.

--- tests/DoctrineTestSuite1.php
+++ tests/DoctrineTestSuite1.php
@@ -1,4 +1,8 @@
 <?php
+// Most of our environments (including Travis) default to (or are configured to) a memory limit of 128M.
+// The test suite goes over this limit.
+// So we increase the limit when running the test suite to 512M.
+ini_set('memory_limit', '512M');
 require_once __DIR__ . '/doctrine/DoctrineCleanInsert1Test.php';
 require_once __DIR__ . '/doctrine/NGIServiceTest.php';
 require_once __DIR__ . '/doctrine/RoleCascadeDeletionsTest.php';
