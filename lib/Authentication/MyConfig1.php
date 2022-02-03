d0699aa9fe6a9a8219c0dcbf392a6850b08bcc43
gregcorbett <gregcorbett@users.noreply.github.com>
2022-02-03 16:44:40 +0000

    Re-enable shib by default, disable IAM by default
    - shib will still be the main SSO usecase for 5.9.0, so I want
      to have that activiated by default.

--- lib/Authentication/MyConfig1.php
+++ lib/Authentication/MyConfig1.php
@@ -35,8 +35,8 @@ class MyConfig1 implements IConfigFirewallComponent {
 
        $this->tokenClassList = array();
        $this->tokenClassList[] = 'org\gocdb\security\authentication\X509AuthenticationToken';
-       $this->tokenClassList[] = 'org\gocdb\security\authentication\IAMAuthToken';
-       //$this->tokenClassList[] = 'org\gocdb\security\authentication\ShibAuthToken';
+       //$this->tokenClassList[] = 'org\gocdb\security\authentication\IAMAuthToken';
+       $this->tokenClassList[] = 'org\gocdb\security\authentication\ShibAuthToken';
        //$this->tokenClassList[] = 'org\gocdb\security\authentication\SimpleSamlPhpAuthToken';
        //$this->tokenClassList[] = 'org\gocdb\security\authentication\UsernamePasswordAuthenticationToken';
     }
