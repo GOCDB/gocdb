0802328ecd8c3f7715a084aaacb12ff58e75b7ca
Ian Neilson <ian.neilson@stfc.ac.uk>
2022-05-10 12:55:41 +0000

    Upgrade to ORM 2.10
    As of ORM 2.10 Doctrine annotations require an exta package called
    doctrine/annotations (requires symfony/cache).
    
    https://www.doctrine-project.org/projects/doctrine-orm/en/2.10/reference/annotations-reference.html and https://github.com/doctrine/orm/blob/2.10.x/UPGRADE.md#upgrade-to-29

--- tests/doctrine/DoctrineCleanInsert1Test.php
+++ tests/doctrine/DoctrineCleanInsert1Test.php
@@ -464,7 +464,7 @@ class DoctrineCleanInsert1Test extends PHPUnit_Extensions_Database_TestCase {
    * first have to delete the services that holds the FK to the site (one site
    * to many services).
    *
-   * @expectedException \Doctrine\DBAL\DBALException
+   * @expectedException \Doctrine\DBAL\Exception
    */
   public function testExpectedFK_ViolationOnSiteDeleteWithoutCascade(){
     print __METHOD__ . "\n";
@@ -496,7 +496,7 @@ class DoctrineCleanInsert1Test extends PHPUnit_Extensions_Database_TestCase {
     // services first as we have no cascade-delete option set.
     $this->em->remove($refetchedSite);
     $this->em->flush();
-    $this->fail('Should not get to this point - DBALException expected');
+    $this->fail('Should not get to this point - \Doctrine\DBAL\Exception expected');
 
   }
 
@@ -659,7 +659,7 @@ class DoctrineCleanInsert1Test extends PHPUnit_Extensions_Database_TestCase {
    * association to a downtime (this relationship would need to be deleted first
    * to allow the endpoint to be deleted cleanly by the cascade).
    *
-   * @expectedException \Doctrine\DBAL\DBALException
+   * @expectedException \Doctrine\DBAL\Exception
    */
   public function testExpectedFK_ViolationOnServiceToEndpointCascadeDelete_WithDTs(){
     print __METHOD__ . "\n";
@@ -707,7 +707,7 @@ class DoctrineCleanInsert1Test extends PHPUnit_Extensions_Database_TestCase {
     // services first as we have no cascade-delete option set.
     $this->em->remove($se);
     $this->em->flush();
-    $this->fail('Should not get to this point - DBALException expected');
+    $this->fail('Should not get to this point - \Doctrine\DBAL\Exception expected');
   }
 
   /**
