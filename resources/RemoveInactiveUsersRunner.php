54754168f505d33d316eea87ae50f0b34a6175ea
gregcorbett <gregcorbett@users.noreply.github.com>
2022-05-27 10:05:11 +0000

    Address low hanging code climate fruit

--- resources/RemoveInactiveUsersRunner.php
+++ resources/RemoveInactiveUsersRunner.php
@@ -4,7 +4,6 @@ require dirname(__FILE__) . '/../lib/Doctrine/bootstrap_doctrine.php';
 require_once dirname(__FILE__) . '/../lib/Gocdb_Services/User.php';
 require_once dirname(__FILE__) . '/../lib/Gocdb_Services/Factory.php';
 
-$em = $entityManager;
 $dql = "SELECT u FROM User u";
 $users = $entityManager->createQuery($dql)->getResult();
 
@@ -21,12 +20,12 @@ foreach ($users as $user) {
 
     $lastLoginDate = $user->getLastLoginDate();
 
-    if ($lastLoginDate){ // null lastLoginDate check
+    if ($lastLoginDate) { // null lastLoginDate check
         $interval = $today->diff($lastLoginDate);
     } else { // This might only be run once, since new users always have field filled.
         echo "User has no lastLoginDate (it may have been a very long time.)\n";
         echo "Deleting user.\n";
-        deleteUser($user, $em);
+        deleteUser($user, $entityManager);
         echo "\n";
         return;
     }
@@ -34,40 +33,40 @@ foreach ($users as $user) {
     $elapsedMonths = (int) $interval->format('%a') / 30;
     echo 'Months elapsed since last login: ' . $elapsedMonths . "\n";
 
-    if ($elapsedMonths > 18){ // Delete user
+    if ($elapsedMonths > 18) { // Delete user
         echo "Deleting user\n";
-        deleteUser($user, $em);
-    }
-    elseif ($elapsedMonths > 17){ // Warn user
+        deleteUser($user, $entityManager);
+    } elseif ($elapsedMonths > 17) { // Warn user
         echo "Requesting user warning email.\n";
         sendWarningEmail($user);
-    }
-    elseif ($elapsedMonths < 17){ // Do Nothing
+    } elseif ($elapsedMonths < 17) { // Do Nothing
         echo "Doing nothing.\n";
     }
 
     echo "\n\n";
 }
 
-$em->flush();
+$entityManager->flush();
 echo "Completed ok: ".date('D, d M Y H:i:s');
 
-function deleteUser($user, $em){
-    $em->getConnection()->beginTransaction();
+function deleteUser($user, $entityManager)
+{
+    $entityManager->getConnection()->beginTransaction();
     try {
-        $em->remove($user);
-        $em->flush();
-        $em->getConnection()->commit();
+        $entityManager->remove($user);
+        $entityManager->flush();
+        $entityManager->getConnection()->commit();
         echo "User deleted.\n";
     } catch (\Exception $e) {
-        $em->getConnection()->rollback();
-        $em->close();
+        $entityManager->getConnection()->rollback();
+        $entityManager->close();
         echo "User not deleted.\n";
         throw $e;
     }
 }
 
-function sendWarningEmail($user){
+function sendWarningEmail($user)
+{
     $emailAddress = $user->getEmail();
     $certDn = $user->getCertificateDn();
 
@@ -77,7 +76,7 @@ function sendWarningEmail($user){
 
     //$webPortalURL = "gocdb-portal-address";
     $localInfoLocation = __DIR__ . "/../config/local_info.xml";
-    $localInfoXML = simplexml_load_file ( $localInfoLocation );
+    $localInfoXML = simplexml_load_file($localInfoLocation);
     $webPortalURL = $localInfoXML->local_info->web_portal_url;
 
     $body = "Dear GOCDB User,\n\n" . "Your account (ID:". $certDn .") has not"
