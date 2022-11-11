8d6bcbbf188c8f19660936b68a377065886920e8
rowan04 <rowanmoss04@gmail.com>
2022-11-11 10:28:36 +0000

    Removes one of the NGIs from EGI Project
    - Removes the NGI_HU NGI from the EGI Project
    - Changes the NGI_HU NGI and its sites to have Local Scope (not EGI)

--- lib/Doctrine/deploy/AddNGIs.php
+++ lib/Doctrine/deploy/AddNGIs.php
@@ -8,6 +8,7 @@ require_once __DIR__."/AddUtils.php";
  * the doctrine prototype.
  * XML format is the output from get_roc_list PI query.
  */
+
 $ngisFileName = __DIR__ . "/" . $GLOBALS['dataDir'] . "/NGIs.xml";
 $ngis = simplexml_load_file($ngisFileName);
 
@@ -17,6 +18,9 @@ $egiProject = $entityManager->getRepository('Project')->findOneBy(array("name" =
 //Find the EGI scope tag
 $egiScope = $entityManager->getRepository('Scope')->findOneBy(array("name" => "EGI"));
 
+//Add Local Scope so specified NGI is not part of EGI project
+$localScope = $entityManager->getRepository('Scope')->findOneBy(array("name" => "Local"));
+
 foreach($ngis as $xmlNgi) {
     $doctrineNgi = new NGI();
     $name = "";
@@ -24,6 +28,7 @@ foreach($ngis as $xmlNgi) {
     $rodEmail = "";
     $helpdeskEmail = "";
     $securityEmail = "";
+    $objectID = "";
     $creationDate = new \DateTime("now");
 
     foreach($xmlNgi as $key => $value) {
@@ -51,15 +56,21 @@ foreach($ngis as $xmlNgi) {
             $securityEmail = (string) $value;
         }
 
+	if((string) $key == "OBJECT_ID") {
+	    $objectID = (string) $value;
+	}
+
         if((string) $key == "CDATEON") {
             // $cdateonString has the following format: '12-JAN-10 14.12.56.000000'
             $cdateonString = (string) $value;
 
             //convert to date time
-            $creationDate = DateTime::createFromFormat('d-M-y G.i.s.u', $cdateonString, new DateTimeZone('UTC'));
+            $creationDate = DateTime::createFromFormat('d-M-y G.i.s.u',
+            $cdateonString, new DateTimeZone('UTC'));
 
             if ($creationDate == false) {
-            throw new LogicException("Datetime in unexpected format. datetime: '" . $cdateonString . "'");
+            throw new LogicException("Datetime in unexpected format. datetime: '" .
+            $cdateonString . "'");
             }
         }
 
@@ -72,16 +83,21 @@ foreach($ngis as $xmlNgi) {
     $doctrineNgi->setHelpdeskEmail($helpdeskEmail);
     $doctrineNgi->setSecurityEmail($securityEmail);
 
+
     // TODO
     //if($cdateon == null) throw new Exception("CDATEON is null");
     //$doctrineNgi->setCreationDate($cdateon);
 
-    $egiProject->addNgi($doctrineNgi);
-
-    $doctrineNgi->addScope($egiScope);
-
-    $entityManager->persist($doctrineNgi);
-
+    // if the NGI has id 67518 (NGI_HU) do not add it to EGI Project
+    if ($objectID == "67518") {
+        $doctrineNgi->addScope($localScope);
+        $entityManager->persist($doctrineNgi);
+    }
+    else {
+        $egiProject->addNgi($doctrineNgi);
+        $doctrineNgi->addScope($egiScope);
+        $entityManager->persist($doctrineNgi);
+    }
 
 }
 
