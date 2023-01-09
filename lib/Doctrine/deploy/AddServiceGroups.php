f1174c12e7b8793ea6aebbb9e441a725d3548114
rowan04 <rowanmoss04@gmail.com>
2023-01-09 16:04:12 +0000

    Add check that the XML file parsed correctly
    - This is to try and cover the case the removed LogicException was trying to catch

--- lib/Doctrine/deploy/AddServiceGroups.php
+++ lib/Doctrine/deploy/AddServiceGroups.php
@@ -10,6 +10,15 @@ require_once __DIR__."/AddUtils.php";
 $stFileName = __DIR__ . "/" . $GLOBALS['dataDir'] . "/ServiceGroups.xml";
 $sts = simplexml_load_file($stFileName);
 
+// Checking the XML file has parsed correctly
+if ($sts === false) {
+    echo "There were errors parsing the XML file.\n";
+    foreach (libxml_get_errors() as $error) {
+        echo $error->message;
+    }
+    exit;
+}
+
 foreach($sts as $st) {
     $instance = new ServiceGroup();
     $name = "";
