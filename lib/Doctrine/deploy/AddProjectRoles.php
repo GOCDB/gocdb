096b8af8d8bb4d47b8cf481849c75e1273ba229f
rowan04 <rowanmoss04@gmail.com>
2023-01-12 13:06:26 +0000

    Fixes users not in EGI project having roles over it
    - Users in NGI_HU which is not a part of the EGI project still had roles over/in the EGI project
    - to fix this, check added in AddProjectRoles.php so that only project roles over the EGI project were added to the EGI project
    - And users in NGI_HU had any roles over the project entity removed, as NGI_HU isn't part of a project

--- lib/Doctrine/deploy/AddProjectRoles.php
+++ lib/Doctrine/deploy/AddProjectRoles.php
@@ -5,33 +5,35 @@ require_once __DIR__."/AddUtils.php";
 
 /**
  * AddEgiRoles.php: Loads a list of roles from the get_user PI
- * query output (XML), finds the EGI roles and inserts them into
- * the doctrine prototype.
+ * query output (XML), finds whether they are EGI roles or not
+ * and if they are, adds them to the EGI project and 
+ * insert them into the doctrine prototype.
+ * If they are not an EGI role, they are not added to a project
  * XML format is the output from get_roc_list PI query, e.g:
 
 
- <EGEE_USER USER_ID=" " PRIMARY_KEY="63777G0">
-        <FORENAME>Patricia</FORENAME>
-        <SURNAME>Gomes</SURNAME>
-        <TITLE>Miss</TITLE>
+ <EGEE_USER USER_ID="468344" PRIMARY_KEY="22434G0">
+        <FORENAME>Yuki</FORENAME>
+        <SURNAME>Tsunoda</SURNAME>
+        <TITLE></TITLE>
         <DESCRIPTION></DESCRIPTION>
         <GOCDB_PORTAL_URL>https://testing.host.com/portal/index.php?Page_Type=View_Object&amp;object_id=113158&amp;grid_id=0</GOCDB_PORTAL_URL>
-        <EMAIL>pgomes@cbpf.br</EMAIL>
+        <EMAIL>Yuki.Tsunoda@izolamrf.si</EMAIL>
         <TEL>+55(21)21417419</TEL>
         <WORKING_HOURS_START></WORKING_HOURS_START>
         <WORKING_HOURS_END></WORKING_HOURS_END>
-        <CERTDN>/C=BR/O=ICPEDU/O=UFF BrGrid CA/O=CBPF/OU=LAFEX/CN=Patricia Gomes</CERTDN>
+        <CERTDN>/C=BR/O=ICPEDU/O=UFF BrGrid CA/O=CBPF/OU=LAFEX/CN=Yuki Tsunoda</CERTDN>
         <APPROVED></APPROVED>
         <ACTIVE></ACTIVE>
-        <HOMESITE></HOMESITE>
+        <HOMESITE>Izola MRF</HOMESITE>
         <USER_ROLE>
-            <USER_ROLE>Regional First Line Support</USER_ROLE>
-            <ON_ENTITY>ROC_LA</ON_ENTITY>
-            <ENTITY_TYPE>group</ENTITY_TYPE>
+            <USER_ROLE>Chief Operations Officer</USER_ROLE>
+            <ON_ENTITY>EGI</ON_ENTITY>
+            <ENTITY_TYPE>project</ENTITY_TYPE>
         </USER_ROLE>
         <USER_ROLE>
-            <USER_ROLE>Site Operations Deputy Manager</USER_ROLE>
-            <ON_ENTITY>CBPF</ON_ENTITY>
+            <USER_ROLE>Site Operations Manager</USER_ROLE>
+            <ON_ENTITY>Izola MRF</ON_ENTITY>
             <ENTITY_TYPE>site</ENTITY_TYPE>
         </USER_ROLE>
     </EGEE_USER>
@@ -86,8 +88,12 @@ foreach($usersRoles as $user) {
             $doctrineUser = $doctrineUser;
         }
 
-        $doctrineRole = new Role($roleType, $doctrineUser, $egi, 'STATUS_GRANTED');
-        $entityManager->persist($doctrineRole);
+        if ((string) $role->ON_ENTITY == "EGI") {
+            $doctrineRole = new Role($roleType, $doctrineUser, $egi, 'STATUS_GRANTED');
+            $entityManager->persist($doctrineRole);
+        } else {
+            continue;
+        }
     }
 }
 
