0726e0c180aceb1a5ccae5f638600b48c6abfb13
gregcorbett <gregcorbett@users.noreply.github.com>
2026-02-09 14:46:13 +0000

    Remove dead internal links

--- htdocs/web_portal/GOCDB_monitor/tests.php
+++ htdocs/web_portal/GOCDB_monitor/tests.php
@@ -51,28 +51,6 @@ $test_desc =  array(
         "Server XML configuration validation."
 );
 
-$test_doc =  array(
-    TEST_1 =>
-        "<a href='https://svn.esc.rl.ac.uk/repos/sct-docs/SCT " .
-        "Documents/Servers and Services/GOCDB/Cookbook and " .
-        "recipes/database_is_down.txt' target='_blank'>" .
-        "documentation/recipe</a>",
-    TEST_2 =>
-        "<a href='https://svn.esc.rl.ac.uk/repos/sct-docs/SCT " .
-        "Documents/Servers and Services/GOCDB/Cookbook and " .
-        "recipes/failover_cookbook.txt' target='_blank'>" .
-        "documentation/recipe</a>",
-    TEST_3 =>
-        "<a href='https://svn.esc.rl.ac.uk/repos/sct-docs/SCT " .
-        "Documents/Servers and Services/GOCDB/Cookbook and " .
-        "recipes/failover_cookbook.txt' target='_blank'>" .
-        "documentation/recipe</a>",
-    TEST_4 =>
-        "<p>Contact GOCDB service managers." .
-        "<br>Other tests have dependencies on the server configuration " .
-        "<br>so may show errors if the configuration is invalid.</p>"
-);
-
 $test_messages =  array(
     TEST_1 => UKNMSG,
     TEST_2 => UKNMSG,
